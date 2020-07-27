1de6f7a7e0ee823544205995f182fc92eeab27e5
IrelDev <ireldeveloper@gmail.com>
2020-07-27 09:03:20 +0500

    Initial Commit

---
+++ TabBarIconGenerator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TabBarIconGenerator
+//
+//  Created by Kirill Pustovalov on 27.07.2020.
+//  Copyright © 2020 Kirill Pustovalov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
