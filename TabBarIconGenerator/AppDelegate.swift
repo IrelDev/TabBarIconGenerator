a43248919162f14f7955eb5a626168934ea977ca
IrelDev <ireldeveloper@gmail.com>
2020-07-27 09:06:18 +0500

    Set applicationShouldTerminateAfterLastWindowClosed to true

--- TabBarIconGenerator/AppDelegate.swift
+++ TabBarIconGenerator/AppDelegate.swift
@@ -29,6 +29,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.contentView = NSHostingView(rootView: contentView)
         window.makeKeyAndOrderFront(nil)
     }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool { true }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
